90ad059db23289028b68caab6d14659eba3aa0cc
Ubuntu <ubuntu@ip-10-147-196-207.ec2.internal>
2013-07-19 03:33:50 +0000

    Updated grader.js to handle URL Checks.

--- grader.js
+++ grader.js
@@ -23,18 +23,23 @@
 
 */
 
+var util = require("util");
 var fs = require("fs");
 var program = require("commander");
 var cheerio = require("cheerio");
+var rest = require("restler"); 
 
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://arcane-plains-6885.herokuapp.com";
+var TYPE_CHECK_DEFAULT = "URL";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     
     if(!fs.existsSync(instr)) {
 	console.log("%s does not exist. Exiting", instr);
+
 	process.exit(1); //http://nodejs.org/api/process.html#process_process_exit_code  
     }
     
@@ -42,8 +47,47 @@ var assertFileExists = function(infile) {
 };
 
 var cheerioHtmlFile = function(htmlfile) {
+    //console.log(cheerio.load(fs.readFileSync(htmlfile)));
     return cheerio.load(fs.readFileSync(htmlfile));
 };
+
+var cheerioUrl = function() {
+
+    var cheerioHelper = function(url, checksfile) {
+	
+	rest.get(url).on("complete", function(result, response) {
+
+	    if(result instanceof Error) {
+		console.error(util.format(response.message));
+	    }
+	    
+	    loadCheerioUrl(result, checksfile);
+	
+	});
+
+    }
+
+    return cheerioHelper;
+};
+
+var loadCheerioUrl = function(result, checksfile) {
+
+    $ = cheerio.load(result);
+    var checks = loadChecks(checksfile).sort()
+    var out = {};
+//console.log($("h1"));
+//console.log
+    for(var ii in checks) {
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
+    }
+
+    checkJson = out;
+
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+    
+}
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
@@ -54,12 +98,22 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
 
+    console.log($("h1"));
+
     for(var ii in checks) {
 	var present = $(checks[ii]).length > 0;
 	out[checks[ii]] = present;
     }
     
     return out;
+};
+
+var checkUrl = function(url, checksfile) {
+
+    var cheerioHelper = cheerioUrl();
+
+    cheerioHelper(url, checksfile);
+
 };
 
 var clone = function(fn) {
@@ -72,11 +126,24 @@ if(require.main == module) {
     program
       .option("-c, --checks <check_file>", "Path to checks.json", clone(assertFileExists), CHECKSFILE_DEFAULT) 
       .option("-f, --file <html_file>", "Path to index.html", clone(assertFileExists), HTMLFILE_DEFAULT)
+      .option("-u, --url <url>","Url to check",URL_DEFAULT)
+      .option("-t, --type_check <type_check>","Type of check performed (URL or FILE)",TYPE_CHECK_DEFAULT)
       .parse(process.argv);
+    
+    var checkJson;
 
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.type_check === "URL") {
+	checkUrl(program.url, program.checks);
+    }
+    else if(program.type_check === "FILE") {
+	checkJson = checkHtmlFile(program.file, program.checks);
+
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+    else {
+	console.log("type_check unknown - please set to URL or FILE. grader.js --help for options.")
+    }
 
 } else {
     exports.checkHtmlFile = checkHtmlFile;
